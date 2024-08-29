bdcba86602250235f4865ca22e131d78e38b03eb
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-08-29 13:31:21 +0530

    Integrated Paypal flow

--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -13,6 +13,9 @@ import { set } from "react-hook-form";
 import axios from "axios";
 import { toast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
 
 const metadata = {
   title: "Payments Page",
@@ -22,6 +25,15 @@ const metadata = {
 export default function PaymentsPage() {
   const [clientToken, setClientToken] = useState<string | null>(null);
   const [dropinInstance, setDropinInstance] = useState<Dropin | null>(null);
+
+  const [toAddress, setToAddress] = useState(
+    "0xe5548bc565f9c3a05266a7fcb144b0ab0aef3f906b254a647d1854604a0bf69c"
+  );
+  const [aptAmount, setAptAmount] = useState(0.01);
+
+  const [txHash, setTxHash] = useState<null | string>(null);
+
+  const [isPaymentProcessing, setIsPaymentProcessing] = useState(false);
 
   useEffect(() => {
     async function fetchClientToken() {
@@ -57,24 +69,29 @@ export default function PaymentsPage() {
     if (!dropinInstance) return;
 
     try {
+      setIsPaymentProcessing(true);
       const paymentPayload = await dropinInstance.requestPaymentMethod();
 
       const { nonce } = paymentPayload;
 
       const payload = {
         nonce,
-        toAddress: 10000,
-        aptAmount: 0.1,
+        toAddress: toAddress,
+        aptAmount: aptAmount,
       };
 
       const res = await axiosInstance.post("/paypal/checkout", payload);
 
       const data = res.data as {
         message: string;
+        txHash: string;
       };
+
+      setTxHash(data.txHash);
 
       toast({
         title: data.message,
+        description: `Transaction Hash: ${data.txHash}`,
       });
     } catch (err) {
       console.log(err);
@@ -82,6 +99,8 @@ export default function PaymentsPage() {
         title: "An error occurred.",
         variant: "destructive",
       });
+    } finally {
+      setIsPaymentProcessing(false);
     }
   }
 
@@ -92,11 +111,60 @@ export default function PaymentsPage() {
       {!clientToken && <p>Loading...</p>}
 
       <div className="max-w-screen-sm">
+        <div className="grid gap-2 mb-4">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              To Address
+            </Label>
+
+            <Input
+              id="toAddress"
+              value={toAddress}
+              placeholder="0x123..."
+              type="text"
+              autoCapitalize="none"
+              autoComplete="off"
+              autoCorrect="off"
+              disabled={isPaymentProcessing}
+              onChange={(e) => setToAddress(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Amount In APT
+            </Label>
+            <Input
+              id="aptAmount"
+              value={aptAmount}
+              placeholder="0.01"
+              type="number"
+              autoCapitalize="none"
+              autoComplete="off"
+              autoCorrect="off"
+              disabled={isPaymentProcessing}
+              onChange={(e) => setAptAmount(parseFloat(e.target.value))}
+            />
+          </div>
+        </div>
+
         <div id="dropin-container-div"></div>
 
-        <Button onClick={handlePayment} className="w-full">
-          Pay
+        <Button
+          onClick={handlePayment}
+          className="w-full"
+          disabled={isPaymentProcessing}
+        >
+          Pay {aptAmount} APT
         </Button>
+
+        {txHash && (
+          <Card className="mt-4 p-4">
+            <p>
+              <span className="font-bold">Transaction Hash: </span>
+              {txHash}
+            </p>
+          </Card>
+        )}
       </div>
     </DashboardShell>
   );
